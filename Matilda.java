1c30dbdaab5e4da8c6ef880dd4c26f14a0d39c22
xxmuddlesxx <cybergoth@live.ie>
2018-02-20 16:59:22 +0000

    Version Alpha (Pre-release Games-Fleadh build)

---
+++ Matilda.java
@@ -0,0 +1,143 @@
+
+/**
+ * Copyright (c) 2001-2017 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/epl-v10.html
+ */
+package sample;
+
+
+import robocode.HitRobotEvent;
+import robocode.RateControlRobot;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.WinEvent;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+import robocode.util.*;
+
+import java.awt.*;
+import java.awt.geom.*;
+
+
+
+/**
+ * Matilda - Robot built with survivability in mind
+ * 
+ * Lock on to nearest robot, dodge and shoot.
+ *
+ * @author Neal (original)
+ * 
+ */
+public class Matilda extends RateControlRobot {
+	int count = 0; // Keeps track of turn count
+	double gunTurnAmt; // gun Turn value
+	String trRobotName; // the Name of the Robot currently tracking
+	/**
+	 * run:  Tracker's main run function
+	 */
+	public void run() {
+		// Set colors
+		setBodyColor(new Color(200, 128, 50));
+		setGunColor(new Color(100, 50, 20));
+		setRadarColor(new Color(50, 50, 70));
+		setScanColor(Color.red);
+		setBulletColor(Color.green);
+
+		// Gun vars - Design defaults (Neal)
+		trRobotName = null; // Forget Tracking for now.. 
+		setAdjustGunForRobotTurn(true); // No change in Gun direction on change in direction
+		gunTurnAmt = 180; // Set gun Turn to max of 45 degrees.
+		
+		// Loop forever
+		while (true) {
+			// turn the Gun + turn counter
+			turnGunRight(gunTurnAmt);
+			count++;
+			if (count % 10 == 0) {
+				trRobotName = null;
+			}	
+  		}
+	}	
+	/**
+	 * onScannedRobot:  triggers on any time the radar catches an enemy robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Priority Queue - Targetting, Wall Evasion, Target Reacquisition
+		double bulletPower = Math.min(3.0,getEnergy());
+		double myX = getX();
+		double myY = getY();
+		double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
+		double enemyX = getX() + e.getDistance() * Math.sin(absoluteBearing);
+		double enemyY = getY() + e.getDistance() * Math.cos(absoluteBearing);
+		double enemyHeading = e.getHeadingRadians();
+		double enemyVelocity = e.getVelocity();
+		double gunTurnAmt = 360; // Set gun Turn one whole rotation
+			double previousEnergy = 100;
+		
+			//The Enemy has fired a bullet
+		
+		
+		if (e.isSentryRobot() == true){
+		setTurnRadarRight(999);
+		setTurnGunRight(999);
+		}
+		else{
+	
+		 
+		 
+		double deltaTime = 0;
+		double battleFieldHeight = getBattleFieldHeight(), 
+		       battleFieldWidth = getBattleFieldWidth();
+		double predictedX = enemyX, predictedY = enemyY;
+			while((++deltaTime) * (18.0 - 3.0 * bulletPower) < 
+				  Point2D.Double.distance(myX, myY, predictedX, predictedY)){		
+				predictedX += Math.sin(enemyHeading) * enemyVelocity;	
+				predictedY += Math.cos(enemyHeading) * enemyVelocity;
+				if(	predictedX < 18.0
+					|| predictedY < 18.0
+					|| predictedX > battleFieldWidth - 18.0
+					|| predictedY > battleFieldHeight - 18.0){
+					predictedX = Math.min(Math.max(18.0, predictedX), 
+								battleFieldWidth - 18.0);	
+					predictedY = Math.min(Math.max(18.0, predictedY), 
+								battleFieldHeight - 18.0);
+					break;
+				}
+
+					//angle theta for atan(delta)
+					double theta = Utils.normalAbsoluteAngle(Math.atan2(
+						predictedX - getX(), predictedY - getY()));
+
+					setTurnRadarRightRadians(Utils.normalRelativeAngle(absoluteBearing - getRadarHeadingRadians()));
+					setTurnGunRightRadians(Utils.normalRelativeAngle(theta - getGunHeadingRadians()));
+					fire(bulletPower);
+			}
+		}
+	}
+	/**
+	 * onHitRobot:  Set him as our new target
+	 */
+	public void onHitRobot(HitRobotEvent e) {
+		// Only print if he's not already our target.
+		if (trRobotName != null && !trRobotName.equals(e.getName())) {
+			out.println("Tracking " + e.getName() + " due to collision");
+		}
+		// Set the target
+		trRobotName = e.getName();
+		gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
+		
+	}
+	/**
+	 * Do the worm!!!!
+	 */
+	public void onWin(WinEvent e) {
+		for (int i = 0; i < 50; i++) {
+			turnRight(30);
+			ahead(5);
+			turnLeft(30);
+			ahead(5);
+		}
+	}
+}
